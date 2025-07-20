a6e1bfef9f40965f15fdc5479ec6b6495a5c43f9
Nayem Islam <gangsn490@gmail.com>
2025-07-20 16:05:56 +0600

    (feat): create all requierment folder

---
+++ src/pages/public/tracks/TracksView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TracksView = () => {
+  return <div>TracksView</div>;
+};
+
+export default TracksView;
---
+++ src/pages/public/videos/VideosView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideosView = () => {
+  return <div>VideosView</div>;
+};
+
+export default VideosView;
